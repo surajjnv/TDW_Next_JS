b141a60f43a601eaea70723218c520694baea811
Suraj Gahatyari <suraj.gahatyari@indiamart.com>
2025-02-25 16:01:58 +0530

    category page changes

---
+++ tdw_nextjs/pages/Components/IsqSection.jsx
@@ -0,0 +1,24 @@
+export default function IsqSection ({prd}){
+    let isqShown = 0;
+    if (prd?.ISQ || prd.ISQ.length !== 0) 
+    return(<>
+    {
+        prd.ISQ.map((item, index) => {
+          if (isqShown >= 10) return null; // Limit to 10 rows
+          if (item.FK_IM_SPEC_MASTER_DESC && item.SUPPLIER_RESPONSE_DETAIL) {
+            isqShown++;
+            return (
+              <tr key={index}>
+                <td className="fw6">{item.FK_IM_SPEC_MASTER_DESC}</td>
+                <td>{item.SUPPLIER_RESPONSE_DETAIL}</td>
+              </tr>
+            );
+          }
+          return null;
+        })
+
+    }
+    
+    </>)
+    
+}
