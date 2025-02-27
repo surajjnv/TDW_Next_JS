f1e23a3b929ceb2b716746883ba20ba2e100ac76
Suraj Gahatyari <suraj.gahatyari@indiamart.com>
2025-02-27 12:16:49 +0530

    category changes

--- tdw_nextjs/pages/Components/Category/Category.jsx
+++ tdw_nextjs/pages/Components/Category/Category.jsx
@@ -1,21 +1,18 @@
 import "@/styles/category.css";
 import ProductList from './Productlist';
 import ProductNavigation from "./Product_navigation";
-import BreadCrumb from "../BreadCrumb";
 import CategoryDescription from "./Default_Description";
-// import "@styles/category.css";
+import { useEffect, useRef, useState } from "react";
+
 export default function Category({ companydata }) {
     let cat_name = companydata?.DATA?.PRDSERV[0].CAT_NAME == 'New Items' ? companydata?.DATA?.PRDSERV[0].CAT_NAME : companydata?.DATA?.PRDSERV[0].CATDETAIL.CAT_NAME;
     let cat_desc = '';
     cat_desc = companydata?.DATA?.PRDSERV[0].CATDETAIL.CAT_DESC;
-    // console.log("cat_desc");
-    // console.log(cat_desc);
     var bizType = companydata?.DATA?.COMPANYDETAIL?.BIZ;
     let city = companydata?.DATA?.COMPANYDETAIL?.DIR_SEARCH_CITY;
     let country = companydata?.DATA?.COMPANYDETAIL?.DIR_SEARCH_COUNTRY;
     let catId = companydata?.DATA?.PRDSERV[0]?.CAT_ID;
     var prodList = companydata?.DATA?.PRDSERV[0].PRODLIST;
-    // console.log(prodList);
     if (/<table/i.test(cat_desc)) {
         cat_desc = cat_desc
             .replace(/<table/gi, '<div class="text txt7"><div class="tbl1"><table')
@@ -27,10 +24,20 @@ export default function Category({ companydata }) {
         console.log(cat_desc);
 
     }
+
+    const [readMore,setReadMore]= useState(false);
+    const divref = useRef(null);  
+    useEffect(()=>{
+        if(divref.current.scrollHeight > 10){
+            setReadMore(true);
+        }
+    },[])
+    cat_desc = 'Ensure your paint markers last longer and perform like new with our Paint Marker Replacement Tip. This affordable and eco-friendly solution allows you to replace worn-out tips instead of buying new markers, saving you money in the long run. Perfect for professional artists, DIY enthusiasts, and industrial use, these replacement tips are compatible with a wide range of paint markers. Easy to install and durable, they ensure a smooth, consistent flow of paint, providing precision and control with every stroke.Features:Cost-Effective: Extend the life of your paint markers and save money.Durable & Reliable: High-quality material for long-lasting performance.Easy to Install: Simple and quick replacement to get your marker working again.Eco-Friendly: Reduce waste by reusing your existing markers.Versatile: Fits most standard paint markers, perfect for art, crafts, and professional applications.Upgrade your supplies while cutting costs with our Paint Marker Replacement Tip. Keep your projects on budget without compromising on quality!Dear Customers, Now shop from your favorite store Royal Stationers right from your  mobile. Get your produ'
+
+
     return (
         <>
             <div className="ps-home ps-home--5 pdp">
-                {/* <BreadCrumb Catname={cat_name}></BreadCrumb> */}
                 <div className="ps-home__content pdp">
                     <div className="container">
                         <div className="row">
@@ -39,10 +46,23 @@ export default function Category({ companydata }) {
                             </div>
                             <div className="col-md-12">
                                 <p
-                                    className="clr3 lh24 hgt100 overflow-hidden position-relative he06"
+                                    ref ={divref} className={readMore ? "clr3 lh24 hgt100 overflow-hidden position-relative he06" : "clr3 lh24 overflow-hidden position-relative he06"}
                                     id="v_more"
-                                    dangerouslySetInnerHTML={{ __html: cat_desc }}
-                                ></p>
+                                >
+                                    {cat_desc}
+                                </p>
+                                {readMore && 
+                                    <span className="position-absolute">
+                                    <span
+                                    className="cp clr5 fs14 fw-bold"
+                                    onClick={() => {
+                                        setReadMore(false);
+                                    }}
+                                    >
+                                    Read More
+                                    </span>
+                                </span>
+                                }
                             </div>
                         </div>
                         <div className="row mt15">
--- tdw_nextjs/pages/Components/Category/Productlist.jsx
+++ tdw_nextjs/pages/Components/Category/Productlist.jsx
@@ -3,7 +3,7 @@ import Product from "./Product";
 
 
 function ProductList({ companydata }) {
-  console.log("prd page company data", companydata);
+  // console.log("prd page company data", companydata);
   var prdList = companydata?.DATA?.PRDSERV[0]?.PRODLIST;
   console.log(prdList);
 
